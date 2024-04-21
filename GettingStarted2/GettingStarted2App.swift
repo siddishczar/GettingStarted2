63aee89984613c4919fc55043d56bc9565fb3a26
siddishczar <siddarvind04@gmail.com>
2024-04-21 10:34:00 -0400

    Initial Commit

---
+++ GettingStarted2/GettingStarted2App.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStarted2App.swift
+//  GettingStarted2
+//
+//  Created by Sidd Arvind on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GettingStarted2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
